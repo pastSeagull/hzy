d2aaba54fc3b3fa868dfa6bedecb88d70c79208e
暗夜永恒 <977504087@qq.com>
2021-04-05 23:20:32 +0800

    ui

---
+++ leaf-ui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <NavMenu/>
+    <HelloWorld/>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavMenu from "./components/NavMenu";
+
+export default {
+  name: 'app',
+  components: {
+    NavMenu,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
