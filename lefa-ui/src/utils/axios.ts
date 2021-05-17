613b1ff5d82c699912ea33041f2e51ade83ee3dd
Gaviota <2567570935@qq.com>
2021-05-17 11:04:36 +0800

    add leaf-ui

---
+++ lefa-ui/src/utils/axios.ts
@@ -0,0 +1,45 @@
+import Axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+const baseURL = 'https://api.github.com'
+
+const axios = Axios.create({
+  baseURL,
+  timeout: 20000
+})
+
+// 前置拦截器（发起请求之前的拦截）
+axios.interceptors.request.use(
+  (response) => {
+    /**
+     * config 做处理
+     */
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// 后置拦截器（获取到响应时的拦截）
+axios.interceptors.response.use(
+  (response) => {
+    /**
+        response 和 error 做处理
+     */
+    return response
+  },
+  (error) => {
+    if (error.response && error.response.data) {
+      const code = error.response.status
+      const msg = error.response.data.message
+      ElMessage.error(`Code: ${code}, Message: ${msg}`)
+      console.error(`[Axios Error]`, error.response)
+    } else {
+      ElMessage.error(`${error}`)
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default axios
